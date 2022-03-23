18fd8f4f941c77155a891cabb07bd3634d8d4aea
gyowoo1113 <yereube@naver.com>
2022-03-23 21:31:24 +0900

    Refactor: rigidbody.position => rigidbody.MovePosition, Add SetPlayerMovement, MovePlayer

--- Assets/CommonScripts/CharacterMove.cs
+++ Assets/CommonScripts/CharacterMove.cs
@@ -6,7 +6,11 @@ public class CharacterMove : MonoBehaviour
 {
   public float moveSpeed = 10.0f;
   Rigidbody body;
-  
+
+  private float fHorizontal;
+  private float fVertical;
+  private Vector3 playerMovement;
+
   void Start()
   {
      body = GetComponent<Rigidbody>();
@@ -14,7 +18,22 @@ public class CharacterMove : MonoBehaviour
   
   void FixedUpdate()
   {  
-   body.position += new Vector3(Input.GetAxis("Horizontal"),0 , Input.GetAxis("Vertical"))
-    * moveSpeed * Time.fixedDeltaTime;
+    SetPlayerMovement();
+    MovePlayer();
+  }
+
+  void SetPlayerMovement()
+  {
+    fHorizontal = Input.GetAxis("Horizontal");
+    fVertical = Input.GetAxis("Vertical");
+    
+    playerMovement.Set(fHorizontal,0,fVertical);
+  }
+
+  void MovePlayer()
+  {
+    playerMovement = playerMovement.normalized*moveSpeed*Time.fixedDeltaTime;
+    body.MovePosition(transform.position + playerMovement);
+  }
   }
 }

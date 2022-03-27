44d39696bdb917ac36a56e34ab8b10c85f4b9c1d
kth4540 <43128350+kth4540@users.noreply.github.com>
2022-03-27 18:35:06 +0900

    Character Rotation
    캐릭터 방향전환

--- Assets/CommonScripts/CharacterMove.cs
+++ Assets/CommonScripts/CharacterMove.cs
@@ -6,6 +6,8 @@ public class CharacterMove : MonoBehaviour
 {
   public float moveSpeed = 10.0f;
   Rigidbody body;
+
+  public Vector3 Dir;
   
   void Start()
   {
@@ -14,7 +16,20 @@ public class CharacterMove : MonoBehaviour
   
   void FixedUpdate()
   {  
-   body.position += new Vector3(Input.GetAxis("Horizontal"),0 , Input.GetAxis("Vertical"))
-    * moveSpeed * Time.fixedDeltaTime;
+
+    Dir=new Vector3(Input.GetAxis("Horizontal"),0 , Input.GetAxis("Vertical"));
+  //  body.position += new Vector3(Input.GetAxis("Horizontal"),0 , Input.GetAxis("Vertical"))
+  //   * moveSpeed * Time.fixedDeltaTime;
+
+  body.position+=Dir*moveSpeed*Time.fixedDeltaTime;
+
+  if(Dir!=Vector3.zero)
+  {
+    float angle=Mathf.Atan2(Dir.x,Dir.z)*Mathf.Rad2Deg;
+
+    //body.rotation=Quaternion.Euler(0,angle,0);
+
+    body.rotation = Quaternion.Slerp(body.rotation, Quaternion.Euler(0, angle, 0), moveSpeed * Time.fixedDeltaTime);
+  }
   }
 }

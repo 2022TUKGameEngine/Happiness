9b9d91a2f1b8eb8a814f1817b0ad43300f1e60fd
gyowoo1113 <yereube@naver.com>
2022-03-28 00:33:41 +0900

    Feat #2: Collider 영역에서 키 입력 상호작용, Add Log

--- Assets/CommonScripts/FadeInOut.cs
+++ Assets/CommonScripts/FadeInOut.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class FadeInOut : MonoBehaviour
 {
 
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            if(Input.GetKeyUp(KeyCode.Q))
+            {
+                print("check");
+            }
+        }
+    }
 }

f034439161fbe204cbac772ee3ed063ba1cbdfdf
SH-Broar / Ephxod <ephxod01@naver.com>
2022-03-30 22:38:42 +0900

    Prepare for make GameManager
    w/ Farm
    
    Update SampleMap.unity

---
+++ Assets/CommonScripts/Debug/DebugMode.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugMode : MonoBehaviour
+{
+
+    public GameObject debugText;
+    public bool debugMode;
+    public GameObject player;
+
+    void Start()
+    {
+        if (debugMode)
+            debugText.SetActive(true);
+        else
+            debugText.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        if (debugMode)
+        {
+            if (Input.GetKeyDown(KeyCode.PageUp))
+            {
+                player.GetComponent<CharacterMove>().moveSpeed += 0.05f;
+            }
+            if (Input.GetKeyDown(KeyCode.PageDown))
+            {
+                player.GetComponent<CharacterMove>().moveSpeed -= 0.05f;
+            }
+            
+            //채소 심기
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                
+            }
+        }
+    }
+}

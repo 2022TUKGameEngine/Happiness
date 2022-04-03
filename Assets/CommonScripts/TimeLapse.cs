d0cb318b84118999c84eca1272c705e0a085c296
kth4540 <43128350+kth4540@users.noreply.github.com>
2022-04-03 19:34:58 +0900

    Count Day
    Directional Light 1회전당 1일 카운트
    회전 기준으로 밤/낮 판정

--- Assets/CommonScripts/TimeLapse.cs
+++ Assets/CommonScripts/TimeLapse.cs
@@ -7,19 +7,43 @@ public class TimeLapse : MonoBehaviour
     public GameObject dirLight;
     public float TimeForOneDayByMinutes;
     Vector3 angleFactor;
+    public bool DayOrNight;
+    public float CountDay;
+
+    public float AngleX;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        angleFactor = new Vector3(0, -90, 0);
+        angleFactor = new Vector3(0, 0, 0);
+        DayOrNight = true;
+        CountDay = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //360.f / (angleFactor.x % 360) = 현재시각(-25%)
+
+        AngleX=angleFactor.x;
+
         angleFactor.x += Time.deltaTime * 360f / 60f / TimeForOneDayByMinutes;
-        angleFactor.y += Time.deltaTime * 360f / 60f / TimeForOneDayByMinutes;
+        //angleFactor.y += Time.deltaTime * 360f / 60f / TimeForOneDayByMinutes;
+
         dirLight.transform.eulerAngles = angleFactor;
+
+        if(AngleX>180f)
+            DayOrNight=false;
+
+        if(AngleX>360f)
+        {
+            DayOrNight=true;
+            CountDay+=1;
+            angleFactor.x=0;
+        }
+
+
+
+
     }
 }

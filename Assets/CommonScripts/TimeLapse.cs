bcbd72705055dc6b3cb74fccff43a89777611a4a
kth4540 <43128350+kth4540@users.noreply.github.com>
2022-04-05 20:29:35 +0900

    시/분 추가
    Directional Light 각도에 따라 시/분 변경

--- Assets/CommonScripts/TimeLapse.cs
+++ Assets/CommonScripts/TimeLapse.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 
 public class TimeLapse : MonoBehaviour
 {
@@ -9,18 +11,47 @@ public class TimeLapse : MonoBehaviour
     
     Vector3 angleFactor;
 
+    public int Hour;
+    public int Minute;
+
+    public bool DayOrNight;
+    public float CountDay;
+    public float AngleX;
+
+
     // Start is called before the first frame update
     void Start()
     {
         angleFactor = new Vector3(0, -90, 0);
+        DayOrNight=true;
+        CountDay=1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //360.f / (angleFactor.x % 360) = ÇöÀç½Ã°¢(-25%)
+        AngleX=angleFactor.x;
+
+        //360.f / (angleFactor.x % 360) = ï¿½ï¿½ï¿½ï¿½Ã°ï¿½(-25%)
         angleFactor.x += Time.deltaTime * 360f / 60f / TimeForOneDayByMinutes;
-        angleFactor.y += Time.deltaTime * 360f / 60f / TimeForOneDayByMinutes;
-        dirLight.transform.eulerAngles = angleFactor;
+        //angleFactor.y += Time.deltaTime * 360f / 60f / TimeForOneDayByMinutes;
+        
+        dirLight.transform.localEulerAngles=angleFactor;
+
+        if(AngleX>180f)
+            DayOrNight=false;
+
+        if(AngleX>360f)
+        {
+            DayOrNight=true;
+            CountDay+=1;
+            angleFactor.x=0;
+        }
+
+        Hour=(int)(AngleX/15);
+        Minute=(int)(AngleX%15*4);
+
+
+
     }
 }

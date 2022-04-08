087ad81ec50c929fb4e9f9eb04b60736803532b0
SH-Broar / Ephxod <ephxod01@naver.com>
2022-04-09 02:54:27 +0900

    Puff Effect x2

--- Assets/CommonScripts/Character/PuffEvent.cs
+++ Assets/CommonScripts/Character/PuffEvent.cs
@@ -5,18 +5,23 @@ using UnityEngine.VFX;
 
 public class PuffEvent : MonoBehaviour
 {
-    public VisualEffect LvisualEffect;
+    public VisualEffect[] LvisualEffect;
     public Transform LeftToe;
-    public VisualEffect RvisualEffect;
+    public VisualEffect[] RvisualEffect;
     public Transform RightToe;
+
+    int lBuffer = 0;
+    int rBuffer = 0;
     void Lpuff()
     {
-        LvisualEffect.transform.position = new Vector3(LeftToe.position.x, LeftToe.position.y, LeftToe.position.z);
-        LvisualEffect.Play();
+        LvisualEffect[lBuffer].transform.position = new Vector3(LeftToe.position.x, LeftToe.position.y, LeftToe.position.z);
+        LvisualEffect[lBuffer].Play();
+        lBuffer ^= 1;
     }
     void Rpuff()
     {
-        RvisualEffect.transform.position = new Vector3(RightToe.position.x, RightToe.position.y, RightToe.position.z);
-        RvisualEffect.Play();
+        RvisualEffect[rBuffer].transform.position = new Vector3(RightToe.position.x, RightToe.position.y, RightToe.position.z);
+        RvisualEffect[rBuffer].Play();
+        rBuffer ^= 1;
     }
 }

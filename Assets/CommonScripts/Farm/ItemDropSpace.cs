be4073f9d1dad55d86620ad7e5da00061f38cacc
SH-Broar / Ephxod <ephxod01@naver.com>
2022-04-12 19:03:31 +0900

    Random Item Drop System ( 2 / 2 )

--- Assets/CommonScripts/Farm/ItemDropSpace.cs
+++ Assets/CommonScripts/Farm/ItemDropSpace.cs
@@ -6,13 +6,38 @@ public class ItemDropSpace : MonoBehaviour
 {
     public int spaceID;
     public List<ITEM_TYPE> dropItemTypes;
+    public List<int> dropItemGrades;
     public List<int> dropItemRatio;
+
+    private float AccumulatedRatio;
+
+    private void Awake()
+    {
+        if (dropItemTypes.Count != dropItemGrades.Count || dropItemTypes.Count != dropItemRatio.Count)
+        {
+            Debug.LogError("ItemDropSpace.cs: dropItemTypes, dropItemGrades, dropItemRatio must have same length.");
+        }
+
+        AccumulatedRatio = 0;
+        foreach (var r in dropItemRatio)
+        {
+            AccumulatedRatio += r;
+        }
+    }
 
 
     public (ITEM_TYPE,int) whichItemDrop(GameObject item)
     {
-        
-        return (ITEM_TYPE.Fish,1);
+        float gachaCatcher = Random.Range(0, AccumulatedRatio);
+        foreach(var r in dropItemRatio)
+        {
+            if (gachaCatcher < r)
+            {
+                return (dropItemTypes[dropItemRatio.IndexOf(r)], dropItemGrades[dropItemRatio.IndexOf(r)]);
+            }
+            gachaCatcher -= r;
+        }
+        return (dropItemTypes[dropItemRatio.Count - 1], dropItemGrades[dropItemRatio.Count - 1]);
     }
     
 }

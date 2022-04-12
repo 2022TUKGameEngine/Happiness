5f250360e4f85b8caac581d1e82205961ac3ec9e
gyowoo1113 <yereube@naver.com>
2022-04-12 19:02:53 +0900

    Style #5: collider radius 변경, Log 제거

--- Assets/CommonScripts/EventComponent.cs
+++ Assets/CommonScripts/EventComponent.cs
@@ -67,7 +67,6 @@ public class EventComponent : MonoBehaviour
         if (_fishTimer > _waitingTime)
         {
             InventorySystem.instance.GetItem(ITEM_TYPE.Fish, 1);
-            print("GetItems!");
             InitFishTimer();
         }
     }

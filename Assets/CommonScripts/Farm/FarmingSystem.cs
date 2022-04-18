dd0cf670c3240f302e5fcec5b409b3662253585e
SH-Broar / Ephxod <ephxod01@naver.com>
2022-04-19 01:30:52 +0900

    Fix Farming System
    EventCollider move to OutsideCollide
    Fix Error Fox Clear Every Farm ground

--- Assets/CommonScripts/Farm/FarmingSystem.cs
+++ Assets/CommonScripts/Farm/FarmingSystem.cs
@@ -148,15 +148,18 @@ public class FarmingSystem : MonoBehaviour
         print("clear!");
         foreach(var f in currentUseGrounds)
         {
-            if (f.growthTime < 0)
+            if (f.groundNum == groundNum)
             {
-                if (f.growthTime == -2)
+                if (f.growthTime < 0)
                 {
-                    f.getItem();
+                    if (f.growthTime == -2)
+                    {
+                        f.getItem();
+                    }
+                    f.berry.SetFloat("LifetimeBush", 0f);
+                    f.berry.SetFloat("LifetimeBerry", 0f);
+                    Destroy(f.berry.transform.parent.gameObject);
                 }
-                f.berry.SetFloat("LifetimeBush", 0f);
-                f.berry.SetFloat("LifetimeBerry", 0f);
-                Destroy(f.berry.transform.parent.gameObject);
             }
         }
 

acae913a884db73af62f8089e5203a8df1bfdd5d
gyowoo1113 <yereube@naver.com>
2022-04-20 19:05:37 +0900

    Feat : Mining Skeleton, Fish folder Rename => Fishing

---
+++ Assets/CommonScripts/Mining/MiningEvent.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiningEvent : EventComponent
+{
+
+    public override void TriggerEvent()
+    {
+
+    }
+    
+    protected override void Update()
+    {
+        
+    }
+}

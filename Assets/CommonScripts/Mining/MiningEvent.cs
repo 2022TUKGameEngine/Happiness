11eba08c2518362cb3afdc7b2ef07a75e7d1e4b1
gyowoo1113 <yereube@naver.com>
2022-05-14 20:59:25 +0900

    Test : rock objects 상호작용 시 scale 감소하도록 변경 (작업중)
     static 문제 해결 후 테스트 해본 결과 정상적으로 적용됨
     Prefab에 스크립트 추가 및 작업필요

--- Assets/CommonScripts/Mining/MiningEvent.cs
+++ Assets/CommonScripts/Mining/MiningEvent.cs
@@ -7,7 +7,9 @@ public class MiningEvent : EventComponent
 
     public override void TriggerEvent()
     {
-
+        print("mine");
+        Vector3 val = new Vector3(0.2f,0.2f,0.2f);
+        transform.localScale -= val;
     }
     
     protected override void Update()

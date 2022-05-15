b711d14a137acaf1582da50f567fe64e844fce37
gyowoo1113 <yereube@naver.com>
2022-05-15 16:27:33 +0900

    Feat : 낚시와 유사한 방식으로 채광하기 (진행중)
     : scale 값 넘으면 setActive false 필요
     실제로 코드 테스트는 안해봄

--- Assets/CommonScripts/Mining/MiningEvent.cs
+++ Assets/CommonScripts/Mining/MiningEvent.cs
@@ -5,15 +5,54 @@ using UnityEngine;
 public class MiningEvent : EventComponent
 {
 
+    private bool isMining = false;
+    private float _waitingTime = 0.0f;
+    private float _miningTimer = 0.0f;
+    private float _playerMiningDuration = 0.0f;
+    private float _scaleVal = 0.0f;
+
+    public float minMining = 3.0f;
+    public float maxMining = 5.0f;
+    
     public override void TriggerEvent()
     {
-        print("mine");
-        Vector3 val = new Vector3(0.2f,0.2f,0.2f);
-        transform.localScale -= val;
+        isMining = !isMining;
+
+        if (isMining == false)
+        {
+            return;
+        }
     }
     
     protected override void Update()
     {
-        
+        if (isMining)
+        {
+            doMining();
+        }
+    }
+
+    private void doMining()
+    {
+        _miningTimer += Time.deltaTime;
+        _playerMiningDuration += Time.deltaTime;
+
+        if (_miningTimer > _waitingTime)
+        {
+            InventorySystem.instance.GetItem(ITEM_TYPE.Ore, 1);
+            InitMiningTimer();
+        }
+
+        _scaleVal += Time.deltaTime;
+        float scaleVal = 1.0f - _scaleVal;
+        Vector3 val = new Vector3(scaleVal,scaleVal,scaleVal);
+        transform.localScale = val;
+    }
+
+    private void InitMiningTimer()
+    {
+        _waitingTime = Random.Range(minMining, maxMining);
+        _miningTimer = 0.0f;
+        _playerMiningDuration = 0.0f;
     }
 }
